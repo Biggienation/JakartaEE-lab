6ac97ff8bda78953c5f0503a34f364862eec7468
Alexander <alexpet5888@gmail.com>
2025-03-15 16:54:56 +0100

    add Validations to DTO and description and Date to entity

--- src/main/java/org/mangastore/jee2025/dto/ReturningMangaRequest.java
+++ src/main/java/org/mangastore/jee2025/dto/ReturningMangaRequest.java
@@ -1,11 +1,32 @@
 package org.mangastore.jee2025.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ReturningMangaRequest {
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title must be less than 100 characters")
     private String title;
+
+    @NotNull(message = "Pages is required")
+    @Positive(message = "Pages must be a positive number")
     private int pages;
+
+    @NotBlank(message = "Author is required")
+    @Size(max = 50, message = "Author must be less than 50 characters")
     private String author;
+
+    @NotBlank(message = "ISBN is required")
+    @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
     private String ISBN;
+
+    @Size(max = 200, message = "Description must be less than 200 characters")
     private String description;
+
+    @NotBlank(message = "Date is required")
+    @Size(max = 12, message = "Date must be less than 12 characters")
     private String date;
 
     public ReturningMangaRequest() {}
